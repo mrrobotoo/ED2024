f0bdeb60af8d8f4f596e4e7b145af2c1a6d46f85
jhonsalgado <cuh51239696@cuh.mx>
2024-10-04 07:21:01 -0600

    Se rectificara el codigo

--- Examen/src/com/cuh/PilaParEimpar.java
+++ Examen/src/com/cuh/PilaParEimpar.java
@@ -3,9 +3,7 @@ package com.cuh;
 import java.util.Stack;
 
 public class PilaParEimpar {
-
 	
-     
      public static Stack<Integer> separarParImpar(Stack<Integer> pila) {
 		
 			Stack<Integer>numPares=new Stack<Integer>();
@@ -15,24 +13,17 @@ public class PilaParEimpar {
 			while(!pila.isEmpty()) {
 				int numeros=pila.pop();
 				if (numeros % 2 ==0 ) {
+				numImpares.push(numeros);
 				}
 				System.out.println(pila);
 		}
 			return numImpares;
      }		
-	public static void main(String[] args) {
-	 Stack<Integer> pila = new Stack<>();
-     pila.push(222);
-     pila.push(324);
-     pila.push(675);
-     pila.push(835);
-     pila.push(110);
-     pila.push(444);
-     pila.push(22);
-     pila.push(2);
-	}
+     
+}
+
 		
 		
 	
 
-}
+
