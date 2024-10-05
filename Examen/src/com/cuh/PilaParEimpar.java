b1fa255486b6e74762ba30ec70ceb23ff3c168fa
jhonsalgado <cuh51239696@cuh.mx>
2024-10-04 22:30:48 -0600

    Se creo una pila que nos ayudara a separar numeros pares e impares en orden, al impirmirlos primero van los pares y despues los impares

--- Examen/src/com/cuh/PilaParEimpar.java
+++ Examen/src/com/cuh/PilaParEimpar.java
@@ -1,29 +1,53 @@
 package com.cuh;
-
 import java.util.Stack;
-
 public class PilaParEimpar {
 	
-     public static Stack<Integer> separarParImpar(Stack<Integer> pila) {
+		public static void  main (String[] args) {
+		Stack<Integer> numerosIngresadosPorElSistema = new Stack<Integer>();
 		
-			Stack<Integer>numPares=new Stack<Integer>();
-			Stack<Integer>numImpares=new Stack<Integer>();
+		numerosIngresadosPorElSistema.push(445);
+		numerosIngresadosPorElSistema.push(444);
+		numerosIngresadosPorElSistema.push(221);
+		numerosIngresadosPorElSistema.push(875);
+		numerosIngresadosPorElSistema.push(12);
+		numerosIngresadosPorElSistema.push(75);
+		numerosIngresadosPorElSistema.push(14);
+		numerosIngresadosPorElSistema.push(90);
+		numerosIngresadosPorElSistema.push(888);
+		numerosIngresadosPorElSistema.push(2);
+		
+			//Se llama al metodo parEImpar
+			Stack<Integer> pilaFinal = parEImpar(numerosIngresadosPorElSistema);		
+    }
+			private static Stack<Integer> parEImpar(Stack<Integer> numerosIngresadosPorElSistema) {
+			Stack<Integer> numerosPares = new Stack<Integer>();
+			Stack<Integer> numerosImpares = new Stack<Integer>();
+			Stack<Integer> pilaFinal = new Stack<Integer>();
 			
 			
-			while(!pila.isEmpty()) {
-				int numeros=pila.pop();
-				if (numeros % 2 ==0 ) {
-				numImpares.push(numeros);
-				}
-				System.out.println(pila);
+				while (!numerosIngresadosPorElSistema.isEmpty()) {
+					int numero = numerosIngresadosPorElSistema.pop(); 
+		            if (numero % 2 == 0) { 
+		                numerosPares.push(numero);  
+		            } else {
+		                numerosImpares.push(numero); 
+		            }
+		            //En este while se duardaran primero los pares en la pila final
+		        }
+				while (!numerosPares.isEmpty()) {
+		            pilaFinal.push(numerosPares.pop());
+		        }
+
+		        //En este fuardaremos los impares en la pila final
+		        while (!numerosImpares.isEmpty()) {
+		            pilaFinal.push(numerosImpares.pop());
+		        
+		        
+		        }
+		        //Imprimí el metodo final
+		        System.out.println("Pila en orden de acuerdo al ejercicio ; " + "\n"+ pilaFinal);
+
+		        return pilaFinal; 
+		    }		
 		}
-			return numImpares;
-     }		
-     
-}
-
-		
-		
 	
-
-
