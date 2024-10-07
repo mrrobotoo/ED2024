bdf18641e0adfbfd30cee32e8d5c2638ecf04aab
Cristian <157059561+CrisBM7@users.noreply.github.com>
2024-10-07 16:19:18 -0600

    Puse mas en orden el menu y los nombres de las funciones

--- Examen/src/com/cuh/Tortilleria.java
+++ Examen/src/com/cuh/Tortilleria.java
@@ -4,42 +4,51 @@ import java.util.Scanner;
 import java.util.Stack;
 
 public class Tortilleria {
+	static Scanner scanner = new Scanner(System.in);
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
 		Stack<Persona> personasFila = new Stack<Persona>();
 		Persona persona = new Persona();
-		String nombrePersona;
-		float gramos;
-		float tiempo;
+		String nombrePersona = null;
+		float gramos = 0;
+		float tiempo = 0;
+		boolean opcion = false;
 		
-		mostrarGr();
-		gramos = scanner.nextFloat(); 
-		mostrarTiempo();
-		tiempo = scanner.nextFloat();
-		salir();
-
+		
+		nombrePersona = introducirNombre(nombrePersona);
+		introducirGramos(gramos);
+		introducirTiempo(tiempo);
+		finalizarBucle(opcion);
+		scanner.close();
 	}
 	
 	private void agregarPersona() {
 		
 	}
 
-	private static void mostrarNombre() {
+	private static String introducirNombre(String nombrePersona) {
 		System.out.println("Agregar persona");
 		System.out.print("1. Nombre de la Persona: ");
+		nombrePersona = scanner.nextLine();
+		return nombrePersona;
 	}
 	
-	private static void mostrarGr() {
+	private static float introducirGramos(float gramos) {
 		System.out.print("2. Pon la hora en que llego la persona: ");
+		gramos = scanner.nextFloat();
+		return gramos;
 	}
 
-	private static void mostrarTiempo() {
-		System.out.print("3. Pon cuantos gr de tortilla se llevara: ");
+	private static float introducirTiempo(float tiempo) {
+		System.out.print("3. Pon cuantos gramos de tortilla se llevara: ");
+		tiempo = scanner.nextFloat();
+		return tiempo;
 	}
 	
-	private static void salir() {
-		System.out.println("4.Quieres finalizar la entrada de datos");
-		System.out.println("1 para Si 2 para No");
+	private static boolean finalizarBucle(boolean opcion) {
+		System.out.println("4. Quieres finalizar la entrada de datos");
+		System.out.println("	(1 para Si) (2 para No)");
 		System.out.print("Eliga una opcion: ");
+		opcion = scanner.hasNextBoolean();
+		return opcion;
 	}
 }
