76945f26979fcaaa26a7834a6777ec71a37a8210
Cristian <157059561+CrisBM7@users.noreply.github.com>
2024-10-07 17:58:01 -0600

    Se hicieron cambios en la clase persona y se puso el bucle para introducir datos

--- Examen/src/com/cuh/Persona.java
+++ Examen/src/com/cuh/Persona.java
@@ -6,16 +6,16 @@ public class Persona {
 	private float gr;
 	private float tiempo;
 	
-	public String getNombre() {
+	String getNombre() {
 		return nombre;
 	}
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-	public float getgR() {
+	public float getgr() {
 		return gr;
 	}
-	public void setgR(float gr) {
+	public void setgr(float gr) {
 		this.gr = gr;
 	}
 	public float getTiempo() {
--- Examen/src/com/cuh/Tortilleria.java
+++ Examen/src/com/cuh/Tortilleria.java
@@ -5,50 +5,76 @@ import java.util.Stack;
 
 public class Tortilleria {
 	static Scanner scanner = new Scanner(System.in);
+
 	public static void main(String[] args) {
 		Stack<Persona> personasFila = new Stack<Persona>();
-		Persona persona = new Persona();
+		Stack<Persona> filaBajaPrioridad = new Stack<Persona>();
+		Stack<Persona> filaAltaPrioridad = new Stack<Persona>();
 		String nombrePersona = null;
 		float gramos = 0;
 		float tiempo = 0;
 		boolean opcion = false;
+
+		do {
+			nombrePersona = introducirNombre();
+			gramos = introducirGramos();
+			tiempo = introducirTiempo();
+			agregarPersona(personasFila, nombrePersona, gramos, tiempo);
+			opcion = finalizarBucle(opcion);
+		} while (opcion != false);
 		
+		int tamañoFila = personasFila.size();
 		
-		nombrePersona = introducirNombre(nombrePersona);
-		introducirGramos(gramos);
-		introducirTiempo(tiempo);
-		finalizarBucle(opcion);
+		for (int i = 0; i < tamañoFila; i++) {
+            if(personasFila.peek().getTiempo() > 1) {
+            	System.out.println("Es mayor");
+            }
+        }
+		
 		scanner.close();
-	}
-	
-	private void agregarPersona() {
-		
+
 	}
 
-	private static String introducirNombre(String nombrePersona) {
-		System.out.println("Agregar persona");
-		System.out.print("1. Nombre de la Persona: ");
-		nombrePersona = scanner.nextLine();
-		return nombrePersona;
-	}
-	
-	private static float introducirGramos(float gramos) {
-		System.out.print("2. Pon la hora en que llego la persona: ");
-		gramos = scanner.nextFloat();
-		return gramos;
+	private static void agregarPersona(Stack<Persona> personasFila, String nombre, float gramos, float tiempo) {
+		Persona persona = new Persona();
+		persona.setNombre(nombre);
+		persona.setgr(gramos);
+		persona.setTiempo(tiempo);
+		personasFila.push(persona);
+		System.out.println("Persona agregada a la fila.");
 	}
 
-	private static float introducirTiempo(float tiempo) {
-		System.out.print("3. Pon cuantos gramos de tortilla se llevara: ");
-		tiempo = scanner.nextFloat();
-		return tiempo;
-	}
-	
+	private static String introducirNombre() {
+        System.out.println("Agregar persona");
+        System.out.print("1. Nombre de la Persona: ");
+        return scanner.nextLine();
+    }
+
+    private static float introducirGramos() {
+        System.out.print("2. Pon la hora en que llego la persona: ");
+        return scanner.nextFloat();
+    }
+
+    private static float introducirTiempo() {
+        System.out.print("3. Pon cuantos gramos de tortilla se llevara: ");
+        return scanner.nextFloat();
+    }
+
 	private static boolean finalizarBucle(boolean opcion) {
+		int num;
 		System.out.println("4. Quieres finalizar la entrada de datos");
 		System.out.println("	(1 para Si) (2 para No)");
 		System.out.print("Eliga una opcion: ");
-		opcion = scanner.hasNextBoolean();
+		num = scanner.nextInt();
+		scanner.nextLine();
+		System.out.println();
+		
+		if(num == 1) {
+			opcion = false;
+		} else {
+			opcion = true;
+		}
+		
 		return opcion;
 	}
 }
