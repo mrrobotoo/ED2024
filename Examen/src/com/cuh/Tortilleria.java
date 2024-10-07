a3432cb96a04efc5cd4f2b7c1a2a49a93e6c5ab1
Mario Sánchez <cuh51239674@cuh.mx>
2024-10-07 08:48:05 -0600

    Commit Tortilleria

---
+++ Examen/src/com/cuh/Tortilleria.java
@@ -0,0 +1,30 @@
+package com.cuh;
+import java.util.Stack;
+import java.util.Scanner;
+public class Tortilleria {
+	
+	public static void main (String[] args) {
+		Stack <Persona> menosTortilla = new Stack<>();
+		Stack <Persona> masTortilla = new Stack<>();
+		
+		agregarPersona(menosTortilla, masTortilla, new Persona("Mario, 700g"));
+		agregarPersona(menosTortilla, masTortilla, new Persona("Cris, 100g"));
+		agregarPersona(menosTortilla, masTortilla, new Persona("Ronaldo, 400g"));
+		agregarPersona(menosTortilla, masTortilla, new Persona("David, 300g"));
+		agregarPersona(menosTortilla, masTortilla, new Persona("Pablo David, 500g"));}
+	
+	System.Out.println("Menos gramos:");
+	imprimirPila(menosTortilla);
+	
+	System.Out.println("Mas gramos");
+	imprimirPila(masTortilla);
+	
+	}
+public static void agregarPersonas(Stack<Persona> menosTortilla, Stack<Persona> masTortilla Persona persona);{
+	if 
+}
+
+		
+	}
+
+}
