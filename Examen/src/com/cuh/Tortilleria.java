97d28956865f23b2668cbcc81716d73ce9e69fde
Mario Sánchez <cuh51239674@cuh.mx>
2024-10-08 07:23:28 -0600

    segundo commit, Tortilleria

--- Examen/src/com/cuh/Tortilleria.java
+++ Examen/src/com/cuh/Tortilleria.java
@@ -1,30 +1,36 @@
 package com.cuh;
 import java.util.Stack;
-import java.util.Scanner;
-public class Tortilleria {
-	
-	public static void main (String[] args) {
-		Stack <Persona> menosTortilla = new Stack<>();
-		Stack <Persona> masTortilla = new Stack<>();
-		
-		agregarPersona(menosTortilla, masTortilla, new Persona("Mario, 700g"));
-		agregarPersona(menosTortilla, masTortilla, new Persona("Cris, 100g"));
-		agregarPersona(menosTortilla, masTortilla, new Persona("Ronaldo, 400g"));
-		agregarPersona(menosTortilla, masTortilla, new Persona("David, 300g"));
-		agregarPersona(menosTortilla, masTortilla, new Persona("Pablo David, 500g"));}
-	
-	System.Out.println("Menos gramos:");
-	imprimirPila(menosTortilla);
-	
-	System.Out.println("Mas gramos");
-	imprimirPila(masTortilla);
-	
-	}
-public static void agregarPersonas(Stack<Persona> menosTortilla, Stack<Persona> masTortilla Persona persona);{
-	if 
-}
+public class Tortilleria{
 
-		
+public static void main(String[] args) {
+	        String[] nombres = {"Ana", "Luis", "María", "Carlos", "Elena", "Pedro"};
+	        String[] horas = {"08:00", "09:00", "07:30", "11:15", "10:45", "08:30"};
+	        int[] gramos = {50, 120, 80, 150, 90, 200};
+	     
+	        Stack<String> pilaMenos = new Stack<>();
+	        Stack<String> pilaMas = new Stack<>();
+	        
+	        for (int i = 0; i < nombres.length; i++) {
+	            String persona = "Nombre: " + nombres[i] + ", Hora: " + horas[i] + ", Gramos de tortilla: " + gramos[i];
+	            if (gramos[i] <= 100) {
+	                pilaMenos.push(persona);
+	            } else {
+	                pilaMas.push(persona);
+	            }
+	        }
+	        
+	        System.out.println("Usuarios que necesitan menos de 100 gramos de tortilla:");
+	        while (!pilaMenos.isEmpty()) {
+	            System.out.println(pilaMenos.pop());
+	        }
+	        
+	        System.out.println("Usuarios que necesitan más de 100 gramos de tortilla:");
+	        while (!pilaMas.isEmpty()) {
+	            System.out.println(pilaMas.pop());
+	        }
+	    }
 	}
 
-}
+
+
+
