594fa8f024d8aeb5997ab1a885f0374ebe60757a
jhonsalgado <cuh51239696@cuh.mx>
2024-10-08 23:59:27 -0600

    Se crearon 2 pilas que albergarán los gramos y turnos, el codigo es incompleto y erroneo, requiere de mejoras que se realizaran lo mas antes posible.

---
+++ Examen/src/com/cuh/Tortilleria.java
@@ -0,0 +1,29 @@
+package com.cuh;
+import java.util.Stack;
+
+public class Tortilleria {
+	
+	public static void main(String[] args) {
+    Stack<Double>masGramos = new Stack<Double>();
+    masGramos.push(5.5);
+    masGramos.push(7.23);
+    masGramos.push(8.5);
+    masGramos.push(4.5);
+    masGramos.push(10.5);
+    
+    
+    Stack<Double>menosGramos = new Stack<Double>();
+    menosGramos.push(5.5);
+    menosGramos.push(7.23);
+    menosGramos.push(8.5);
+    menosGramos.push(4.5);
+    menosGramos.push(10.5);
+    
+    Stack<String>turnoOHoraDeLlegada = new Stack<String>();
+    turnoOHoraDeLlegada.push("10:00 am");
+    turnoOHoraDeLlegada.push("10:00 am");
+    turnoOHoraDeLlegada.push("10:00 am");
+    turnoOHoraDeLlegada.push("10:00 am");
+    turnoOHoraDeLlegada.push("10:00 am");
+}
+}
