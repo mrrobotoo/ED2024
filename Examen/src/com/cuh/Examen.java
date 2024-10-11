4e81e3c890d1f5323c568fb3142845e502d1601e
DESKTOP-O4A5GLC\PC-18 <cuh51239814@cuh.mx>
2024-10-11 08:49:32 -0600

    ExamenPablotellez

---
+++ Examen/src/com/cuh/Examen.java
@@ -0,0 +1,41 @@
+package com.cuh;
+
+import java.util.Stack;
+
+
+
+public class Examen {
+
+	public static void main(String[]args) {
+
+		Stack<interger> pila1 = new Stack<interger>();
+		Stack<interger> pila2 = new Stack<interger>();
+		
+		pila1.push(1);
+		pila1.push(2);
+		pila1.push(4);
+		pila1.push(9);
+		pila1.push(2);
+		pila1.push(5);
+		pila1.push(3);
+		
+		pila2.push(4);
+		pila2.push(10);
+		pila2.push(1);
+		pila2.push(4);
+		pila2.push(12);
+		pila2.push(9);
+		pila2.push(3);
+		
+		Stack<interger> pila3resultado = new Stack<interger>();
+		int multiplicarpilas=0;
+		
+		System.out.println("pila1");
+		
+		
+
+		
+	
+		
+	}
+}
