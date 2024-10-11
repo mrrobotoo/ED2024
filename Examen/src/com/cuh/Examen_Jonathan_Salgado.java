38e816013f67272449cd777bdeccc6e7b02db11c
jhonsalgado <cuh51239696@cuh.mx>
2024-10-11 08:49:43 -0600

    Examen_Parcial

---
+++ Examen/src/com/cuh/Examen_Jonathan_Salgado.java
@@ -0,0 +1,37 @@
+package com.cuh;
+import java.util.Stack;
+
+public class Examen_Jonathan_Salgado {
+
+	public static void main(String[] args) {
+		Stack<Integer> pila1 = new Stack<Integer>();
+		Stack<Integer> pila2 = new Stack<Integer>();
+		
+		pila1.push(4);
+		pila1.push(5);
+		pila1.push(7);
+		pila1.push(5);
+		pila1.push(3);
+		pila1.push(2);
+		pila1.push(1);
+		
+		pila2.push(7);
+		pila2.push(4);
+		pila2.push(5);
+		pila2.push(3);
+		pila2.push(2);
+		pila2.push(1);
+		pila2.push(5);
+		
+	int resultado = (pila1*pila2);
+
+		
+		for (int i = 0; i < args.length; i++) {
+		while(resultado == 0) {
+		
+		}	
+		}		
+		
+	
+}
+}
