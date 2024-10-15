e18a023dceab91669637059ce65acd16c7d64426
jhonsalgado <cuh51239696@cuh.mx>
2024-10-15 08:22:22 -0600

    Primera clase de Arreglos

---
+++ Examen/src/com/cuh/Arreglos.java
@@ -0,0 +1,20 @@
+package com.cuh;
+
+public class Arreglos {
+
+	public static void main(String[] args) {
+		int [] numeros;
+	numeros = new int[5];
+	
+	String [] meterDatos= {"hello", "Adele", "are you","my love"};
+	
+	/*System.out.println(meterDatos.length);
+	
+	for (int i = 0; i < meterDatos.length; i++) {
+		System.out.println(meterDatos[i]);
+	}*/
+	meterDatos[0] =10;
+	System.out.println(numeros);
+	}
+
+}
