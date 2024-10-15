451186a96ffac27026bd13d4290483bab5a3ce6b
Cristian <157059561+CrisBM7@users.noreply.github.com>
2024-10-15 15:02:57 -0600

    Cambio a la estructura y en los pasos se podria decir que va a la mitad

--- Examen/src/com/cuh/Arreglo.java
+++ Examen/src/com/cuh/Arreglo.java
@@ -2,36 +2,45 @@ package com.cuh;
 
 public class Arreglo {
 	public static void main(String[] args) {
-		Double renglon1[] = new Double[4];
-		Double renglon2[] = new Double[4];
-		Double renglon3[] = new Double[4];
-
+		Double renglon[][] = new Double[4][4];
+		int p1 = 0;
+		int p2 = 0;
+		double pivote;
+		int entradaDivision = 1;
 		
-		renglon1[0]=5.0;
-		renglon1[1]=2.0;
-		renglon1[2]=1.0;
-		renglon1[3]=20.0;
+		renglon[0][0] = 5.0;
+		renglon[0][1] = 2.0;
+		renglon[0][2] = 1.0;
+		renglon[0][3] = 20.0;
 		
-		renglon2[0]=2.0;
-		renglon2[1]=1.0;
-		renglon2[2]=2.0;
-		renglon2[3]=10.0;
-		//renglon2 - n*reglon1
-		Double pivoteRenglo1 = renglon1[0];
-		Double pivoteRenglo2 = renglon2[0];
-		//String renglon1bonito;
+		renglon[1][0] = 2.0;
+		renglon[1][1] = 1.0;
+		renglon[1][2] = 2.0;
+		renglon[1][3] = 10.0;
 		
-		for(int i = 0; i < renglon1.length ;i++) {
-			renglon1[i] = renglon1[i]/pivoteRenglo1;
-			//renglon1bonito = String.valueOf(renglon1[i]) ;
-			System.out.print(renglon1[i] + " ");
-		}
+		renglon[2][0] = 4.0;
+		renglon[2][1] = 1.0;
+		renglon[2][2] = 3.0;
+		renglon[2][3] = 17.0;
 		
-		System.out.println("");
-		for(int i = 0; i < renglon2.length ;i++) {
-			renglon2[i] = 
-					renglon2[i] - pivoteRenglo2*renglon1[i] ;
-			System.out.print(renglon2[i] + " ");
+		
+		for(int i = 0; i < renglon.length-1 ;i++) {
+			pivote = renglon [p1][p2];
+			for(int j = 0; j < renglon.length; j++) {
+				if(i < entradaDivision) {
+					renglon[i][j] /= pivote;
+					System.out.print(renglon[i][j] + " ");
+				} else {
+					renglon[i][j] -= pivote * renglon[i-p1][j];
+					System.out.print(renglon[i][j] + " ");
+				}
+			}
+			p1++;
+			if(p1 == 3) {
+				p2++;
+				p1 = 0;
+			}
+			System.out.println();
 		}
 		
 	}
