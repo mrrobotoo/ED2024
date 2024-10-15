97c2296e5128c85636e4dfa9007d7b824bfba19c
Cristian <157059561+CrisBM7@users.noreply.github.com>
2024-10-15 12:41:02 -0600

    Examen

---
+++ Examen/src/com/cuh/Solution.java
@@ -0,0 +1,18 @@
+package com.cuh;
+import java.io.*;
+import java.util.*;
+import java.util.Scanner;
+
+public class Solution {
+	  public static void main(String[] args) {
+	    Scanner scanner = new Scanner(System.in);
+	    Stack<Character> operaciones = new Stack<Character>();
+	    String userPush;
+
+	    userPush = scanner.next();
+
+
+
+	    
+	  }
+	}
