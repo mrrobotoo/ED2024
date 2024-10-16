5bfc57fd54212e4ddbb60a7919636326aa7c1e7c
Carlos Ruhal Sebastian Ruiz Perez <sebastianperezruiz6@gmail.com>
2024-10-16 09:06:06 -0600

    Ocupando una matriz

--- Examen/src/com/cuh/Arreglo.java
+++ Examen/src/com/cuh/Arreglo.java
@@ -1,81 +1,50 @@
 package com.cuh;
 
+import java.util.Iterator;
+
 public class Arreglo {
 	    public static void main(String[] args) {
-	        Double[] renglon1 = {3.0,2.0,1.0,1.0};
-	        Double[] renglon2 = {5.0,3.0,4.0,2.0};
-	        Double[] renglon3 = {1.0,1.0,-1.0,1.0};
-	        
-	        renglon1[0] = 3.0;
-	        renglon1[1] = 2.0;
-	        renglon1[2] = 1.0;
-	        renglon1[3] = 1.0;
-	        
-	        renglon2[0] = 5.0;
-	        renglon2[1] = 3.0;
-	        renglon2[2] = 4.0;
-	        renglon2[3] = 2.0;
-	        
-	        renglon3[0] = 1.0;
-	        renglon3[1] = 1.0;
-	        renglon3[2] = -1.0;
-	        renglon3[3] = 1.0;
-
+	        Double[] renglon1 = {5.0,2.0,1.0,20.0};
+	        Double[] renglon2 = {2.0,1.0,2.0,10.0};
+	        Double[] renglon3 = {4.0,1.0,3.0,17.0};
+	
 	        Double[][] renglonLoco = {renglon1, renglon2, renglon3};
 	        
+	        Double pivote;
 	        
 	        
-	        //paso 1
-	        Double pivoteRenglon1 = renglon1[0];
-	        for(int i = 0; i < renglon1.length; i++) {
-	            renglon1[i] /= pivoteRenglon1;
-	        }
+	        for (int i = 0; i < renglonLoco.length; i++) {
+	            pivote = renglonLoco[i][i];
 
-	        // paso2
-	        Double factorRenglon2 = renglon2[0];
-	        for(int i = 0; i < renglon2.length; i++) {
-	            renglon2[i] -= factorRenglon2 * renglon1[i];
-	        }
+	            for (int j = 0; j < renglonLoco[i].length; j++) {
+	                renglonLoco[i][j] /= pivote;
+	            }
 
-	        Double factorRenglon3 = renglon3[0];
-	        for(int i = 0; i < renglon3.length; i++) {
-	            renglon3[i] -= factorRenglon3 * renglon1[i];
+	            for (int k = 0; k < renglonLoco.length; k++) {
+	                if (k != i) {
+	                    Double factor = renglonLoco[k][i];
+	                    for (int j = 0; j < renglonLoco[i].length; j++) {
+	                        renglonLoco[k][j] -= factor * renglonLoco[i][j];
+	                    }
+	                }
+	            }
 	        }
-
-	        // paso 3: 
-	        Double pivoteRenglon2 = renglon2[1];
-	        for(int i = 1; i < renglon2.length; i++) {
-	            renglon2[i] /= pivoteRenglon2;
-	        }
-	        // Hacer ceros  columna 1 para el renglón 1 y 3
-	        Double factorRenglon1B = renglon1[1];
-	        for (int i = 1; i < renglon1.length; i++) {
-	            renglon1[i] -= factorRenglon1B * renglon2[i];
-	        }
-	        Double factorRenglon3B = renglon3[1];
-	        for (int i = 1; i < renglon3.length; i++) {
-	            renglon3[i] -= factorRenglon3B * renglon2[i];
-	        }
-	        Double pivoteRenglon3 = renglon3[2];
-	        renglon3[2] /= pivoteRenglon3;
-	        renglon3[3] /= pivoteRenglon3;
-	        // Hacer ceros  columna 2 para el renglón 1 y 2
-	        Double factorRenglon1C = renglon1[2];
-	        renglon1[2] -= factorRenglon1C * renglon3[2];
-	        renglon1[3] -= factorRenglon1C * renglon3[3];
-	        Double factorRenglon2C = renglon2[2];
-	        renglon2[2] -= factorRenglon2C * renglon3[2];
-	        renglon2[3] -= factorRenglon2C * renglon3[3];
-	        System.out.println("Matriz final:");
-	        imprimirRenglon(renglon1);
-	        imprimirRenglon(renglon2);
-	        imprimirRenglon(renglon3);
+	        
+	        imprimirRenglon(renglonLoco);
+	        
+	        
 	    }
 
-	    public static void imprimirRenglon(Double[] renglon) {
-	        for(Double valor : renglon) {
-	            System.out.printf("%8.2f", valor);
+	    public static void imprimirRenglon(Double[][] renglon) {
+	        
+	    	
+	    	for(Double[] fila : renglon) {
+	    		for(Double valor : fila) {
+		            System.out.printf("%8.2f", valor);
+		        }
+	    		System.out.println();
+	            
 	        }
-	        System.out.println();
+	        
 	    }
 	}
