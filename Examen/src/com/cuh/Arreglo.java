b74ac0f1732ea3852c0f70481e7585415fdbc2e8
Xx0Santiago0xX <cuh51239820@cuh.mx>
2024-10-16 03:03:29 -0600

    Se mejoro el manejo de los pivotes con for y if

---
+++ Examen/src/com/cuh/Arreglo.java
@@ -0,0 +1,107 @@
+package com.cuh;
+
+public class Arreglo {
+	public static void main(String[] args) {
+		Double renglon1[] = new Double[4];
+		Double renglon2[] = new Double[4];
+		Double renglon3[] = new Double[4];
+
+		
+		renglon1[0]=5.0;
+		renglon1[1]=2.0;
+		renglon1[2]=1.0;
+		renglon1[3]=20.0;
+		
+		renglon2[0]=2.0;
+		renglon2[1]=1.0;
+		renglon2[2]=2.0;
+		renglon2[3]=10.0;
+		
+		renglon3[0]=4.0;
+		renglon3[1]=1.0;
+		renglon3[2]=3.0;
+		renglon3[3]=17.0;
+		
+		
+		//renglon2 - n*reglon1
+		Double pivoteRenglo1 = renglon1[0];
+		Double pivoteRenglon2 = renglon2[0];
+		Double pivoteRenglo3 = renglon3[0];
+		Double pivoteRenglon3Final;
+		//String renglon1bonito;
+		
+		for(int i = 0; i < renglon1.length ;i++) {
+			renglon1[i] = renglon1[i]/pivoteRenglo1;
+			//renglon1bonito = String.valueOf(renglon1[i]) ;
+			
+		}
+		
+		for(int i = 0; i < renglon2.length ;i++) {
+			renglon2[i] = renglon2[i] - pivoteRenglon2*renglon1[i] ;
+		}
+		
+		
+		for(int i = 0; i < renglon3.length ;i++) {
+			renglon3[i] = renglon3[i] - pivoteRenglo3*renglon1[i] ;
+		}
+		
+		
+		
+		
+		
+		pivoteRenglon2 = renglon2[1];
+        if (pivoteRenglon2 != 0) {  
+            for (int i = 0; i < renglon2.length; i++) {
+                renglon2[i] = renglon2[i] / pivoteRenglon2;
+          }
+            for (int i = 0; i < renglon1.length; i++) {
+                renglon1[i] = renglon1[i] - renglon2[i] * renglon1[1];
+           }
+         for (int i = 0; i < renglon3.length; i++) {
+                renglon3[i] = renglon3[i] - renglon2[i] * renglon3[1];
+         }
+    }   
+        pivoteRenglon3Final = renglon3[2];
+        if (pivoteRenglon3Final != 0) {  
+            for (int i = 0; i < renglon3.length; i++) {
+            renglon3[i] = renglon3[i] / pivoteRenglon3Final;
+            }
+
+            
+            for (int i = 0; i < renglon1.length; i++) {
+            renglon1[i] = renglon1[i] - renglon3[i] * renglon1[2];
+            }
+
+            for (int i = 0; i < renglon2.length; i++) {
+            renglon2[i] = renglon2[i] - renglon3[i] * renglon2[2];
+            }
+        }
+       
+        for (int i = 0; i < renglon1.length; i++) {
+            System.out.print(renglon1[i] + " ");
+        }
+        System.out.println("");
+
+   
+        for (int i = 0; i < renglon2.length; i++) {
+            System.out.print(renglon2[i] + " ");
+        }
+        System.out.println("");
+     
+        for (int i = 0; i < renglon3.length; i++) {
+            System.out.print(renglon3[i] + " ");
+        }
+        System.out.println("");
+    }
+
+		
+	}
+
+	
+	
+	
+	
+	
+	
+	
+
