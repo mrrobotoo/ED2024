ee57f120fe90ae409d1aee98494b0e22be4b4b61
Xx0Santiago0xX <cuh51239820@cuh.mx>
2024-10-17 00:34:08 -0600

    Añadi el metodo `imprimirRenglon` de la clase main para simplificar la impresión de las matrices y cambie los bucles para que empiecen desde el índice 1, Tambien reduje el codigo repetido al reutilizar los factores pivote.

--- Examen/src/com/cuh/Arreglo.java
+++ Examen/src/com/cuh/Arreglo.java
@@ -27,78 +27,79 @@ public class Arreglo {
 		Double pivoteRenglo1 = renglon1[0];
 		Double pivoteRenglon2 = renglon2[0];
 		Double pivoteRenglo3 = renglon3[0];
-		Double pivoteRenglon3Final;
+		
 		//String renglon1bonito;
 		
 		for(int i = 0; i < renglon1.length ;i++) {
 			renglon1[i] = renglon1[i]/pivoteRenglo1;
 			//renglon1bonito = String.valueOf(renglon1[i]) ;
-			
+			System.out.print(renglon1[i] + " ");
 		}
+		System.out.println(" ");
 		
 		for(int i = 0; i < renglon2.length ;i++) {
 			renglon2[i] = renglon2[i] - pivoteRenglon2*renglon1[i] ;
+			System.out.print(renglon2[i] + " ");
 		}
 		
+		System.out.println(" ");
 		
 		for(int i = 0; i < renglon3.length ;i++) {
-			renglon3[i] = renglon3[i] - pivoteRenglo3*renglon1[i] ;
+			renglon3[i] = renglon3[i] - pivoteRenglo3 * renglon1[i] ;
+			System.out.print(renglon3[i] + " ");
+			
+		}
+		System.out.println(" ");
+		System.out.println(" ");
+		Double FactorRenglon2 = renglon2[1];
+		for(int i = 1; i < renglon2.length ;i++) {
+			renglon2[i] = renglon2[i] / FactorRenglon2;
+			
 		}
 		
+		Double nuevoRenglon3 = renglon3[1];
+		for(int i = 1; i < renglon3.length ;i++) {
+			renglon3[i] = renglon3[i] - nuevoRenglon3 * renglon2[i];	
+		}
 		
-		
-		
-		
-		pivoteRenglon2 = renglon2[1];
-        if (pivoteRenglon2 != 0) {  
-            for (int i = 0; i < renglon2.length; i++) {
-                renglon2[i] = renglon2[i] / pivoteRenglon2;
-          }
-            for (int i = 0; i < renglon1.length; i++) {
-                renglon1[i] = renglon1[i] - renglon2[i] * renglon1[1];
-           }
-         for (int i = 0; i < renglon3.length; i++) {
-                renglon3[i] = renglon3[i] - renglon2[i] * renglon3[1];
-         }
-    }   
-        pivoteRenglon3Final = renglon3[2];
-        if (pivoteRenglon3Final != 0) {  
-            for (int i = 0; i < renglon3.length; i++) {
-            renglon3[i] = renglon3[i] / pivoteRenglon3Final;
-            }
-
-            
-            for (int i = 0; i < renglon1.length; i++) {
-            renglon1[i] = renglon1[i] - renglon3[i] * renglon1[2];
-            }
-
-            for (int i = 0; i < renglon2.length; i++) {
-            renglon2[i] = renglon2[i] - renglon3[i] * renglon2[2];
-            }
-        }
-       
-        for (int i = 0; i < renglon1.length; i++) {
-            System.out.print(renglon1[i] + " ");
-        }
-        System.out.println("");
-
-   
-        for (int i = 0; i < renglon2.length; i++) {
-            System.out.print(renglon2[i] + " ");
-        }
-        System.out.println("");
-     
-        for (int i = 0; i < renglon3.length; i++) {
-            System.out.print(renglon3[i] + " ");
-        }
-        System.out.println("");
-    }
-
-		
+		Double nuevoRenglon1 = renglon1[1];
+		for(int i = 1; i < renglon1.length ;i++) {
+			renglon1[i] = renglon1[i] - nuevoRenglon1 * renglon2[i];
+		}
+		Double pivoteRenglon3 = renglon3[2];
+		for (int i = 2; i < renglon3.length; i++) {
+		    renglon3[i] = renglon3[i] / pivoteRenglon3;
+		}
+		Double nuevoRenglon11 = renglon1[2];
+		for (int i = 2; i < renglon1.length; i++) {
+		    renglon1[i] -= nuevoRenglon11 * renglon3[i];
+		}
+		Double nuevoRenglon22 = renglon2[2];
+		for (int i = 2; i < renglon2.length; i++) {
+		    renglon2[i] -= nuevoRenglon22 * renglon3[i];
+		}
+	imprimirRenglon(renglon1);
+	imprimirRenglon(renglon2);
+	imprimirRenglon(renglon3);
+	}
+	
+	public static void imprimirRenglon(Double[] renglon) {
+	    for (Double valor : renglon) {
+	        System.out.printf("%8.2f", valor);
+	    }
+	    System.out.println();
 	}
 
-	
-	
+	public static void imprimirRenglon(Double[][] renglon) {
+	    for (Double[] fila : renglon) {
+	        for (Double valor : fila) {
+	            System.out.printf("%8.2f", valor);
+	        }
+	        System.out.println();
+	    }
+	    System.out.println();
+	}
+}
 	
 	
 	
