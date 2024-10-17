01002f9c878644b982a80883734da01c474217e5
Evelyn Sanchez <evelynsanchez4965@gmail.com>
2024-10-16 23:55:40 -0600

    commit arreglos

--- Examen/src/com/cuh/Arreglo.java
+++ Examen/src/com/cuh/Arreglo.java
@@ -5,7 +5,6 @@ public class Arreglo {
 		Double renglon1[] = new Double[4];
 		Double renglon2[] = new Double[4];
 		Double renglon3[] = new Double[4];
-
 		
 		renglon1[0]=5.0;
 		renglon1[1]=2.0;
@@ -25,9 +24,7 @@ public class Arreglo {
 		Double pivoteRenglo1 = renglon1[0];
 		Double pivoteRenglo2 = renglon2[0];
 		Double pivoteRenglo3 = renglon3[0];
-		
-		
-		
+
 		
 		//String renglon1bonito;
 		
@@ -46,39 +43,65 @@ public class Arreglo {
 		}
 		System.out.println("");
 		for(int i = 0; i < renglon3.length ;i++) {
-			renglon3[i] = 
-					renglon3[i] - pivoteRenglo3*renglon1[i] ;
+			renglon3[i] = renglon3[i] - pivoteRenglo3*renglon1[i] ;
 			System.out.print(renglon3[i] + " ");
 		}
 		
+	//Matriz 2
+	
 		
 		System.out.print("\n Matriz 2" );
 		System.out.print("\n " );
-		for(int i = 0; i < renglon1.length ;i++) {
-			//renglon1[i] = renglon1[i]/pivoteRenglo1;
-			//renglon1bonito = String.valueOf(renglon1[i]) ;
-			System.out.print(renglon1[i] + " ");
-		}
 		
 		pivoteRenglo2 = renglon2[1];
+		if (pivoteRenglo2 !=0) {
 		System.out.print("\n ");
 		for(int i = 0; i < renglon2.length ;i++) {
 			renglon2[i] = renglon2[i]/pivoteRenglo2 ;
 			System.out.print(renglon2[i] + " ");
 		}
 		
-		pivoteRenglo3 = renglon3[1];
+		pivoteRenglo1 = renglon1[1];
 		System.out.print("\n ");
-		for(int i = 0; i < renglon3.length ;i++) {
-			renglon3[i] = renglon3[i]/pivoteRenglo3 ;
+		for(int i = 0; i < renglon1.length ;i++) {
+			renglon1[i] = renglon1[i] - renglon2[i]*renglon1[1] ;
 			System.out.print(renglon3[i] + " ");
 		}
 		
-		System.out.print("\n Matriz 3" );
-		for(int i = 0; i < renglon1.length ;i++) {
-		System.out.print(renglon1[i] + " ");	
+		pivoteRenglo3 = renglon3[1];
+		System.out.print("\n ");
+		for(int i = 0; i < renglon3.length ;i++) {
+			renglon3[i] = renglon3[i] - renglon2[i]*renglon3[1] ;
+			System.out.print(renglon3[i] + " ");
+		}
 		
+		//Matriz 3
+		System.out.print("\n " );
+		System.out.print("\n Matriz 3" );
+		System.out.print("\n " );
+		
+		
+		Double ultimoPivoteRenglon3 = renglon3[2];
+		if (ultimoPivoteRenglon3 !=0){
+			for(int i = 0; i < renglon3.length ;i++) {
+				renglon3[i] = renglon3[i] / ultimoPivoteRenglon3 ;
+				System.out.print(renglon3[i] + " ");
+			}
 			
+			System.out.print("\n ");
+			for(int i = 0; i < renglon1.length ;i++) {
+				renglon1[i] = renglon1[i] - renglon3[i]*renglon1[2] ;
+				System.out.print(renglon1[i] + " ");
+			}
+	
+			System.out.print("\n ");
+			for(int i = 0; i < renglon2.length ;i++) {
+				renglon2[i] = renglon2[i] - renglon3[i]*renglon2[2] ;
+				System.out.print(renglon2[i] + " ");
+			}
+			
+			
+		}
 		}
 		
 		
@@ -86,9 +109,9 @@ public class Arreglo {
 		
 		
 		
-		 
+		
+	}
+		
 		
 		 }
 		 
-	
-}
