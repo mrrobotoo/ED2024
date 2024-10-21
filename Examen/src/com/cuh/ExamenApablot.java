44ab0a91eceff1740572220dc44a80471b515d42
DESKTOP-O4A5GLC\PC-18 <cuh51239814@cuh.mx>
2024-10-21 08:58:33 -0600

    examenA

---
+++ Examen/src/com/cuh/ExamenApablot.java
@@ -0,0 +1,28 @@
+package com.cuh;
+
+public class ExamenApablot {
+
+	public static void main(String[] args) {
+
+		int[]arreglo1 = {9,8,7,6,5,4,3,2,1,};
+		int[]arreglo2 = {9,8,7,4,5,4,3,2,1,};
+
+
+		
+		for(int i = 0; i < arreglo1.length; i++) {
+		
+		}
+		
+		for(int i = 0; i > arreglo2.length; i++) {
+			if (arreglo2[i] == arreglo1[i]) {
+				
+					System.out.println("no son iguales");
+				}else {
+					System.out.println("son iguales");
+					
+				}
+	
+	}	
+
+	}
+}
